551d7b2fb07da59d8b2b8262ba8d76ba5e8c6f15
Larinayc <56049823+Larinayc@users.noreply.github.com>
2019-10-01 14:22:19 -0700

    Add files via upload

---
+++ D6.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class D6
+{
+    
+    public static void main (String[] args)
+    {
+       System.out.println ("Random die roll generator");
+       double x = Math.random();
+       int y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #1: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #2: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #3: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #4: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #5: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #6: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #7: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #8: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #9: " + y);
+       
+       x = Math.random();
+       y = (int) (x * 6) + 1;
+       System.out.println ("Die roll #10: " + y);
+    }
+}
